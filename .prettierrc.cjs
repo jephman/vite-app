f7919ca3e60cf1c916e23f528d9c6c3941b64cb4
Jeph Manaois <22217016+jephman@users.noreply.github.com>
2022-12-22 03:33:10 +0800

    Initial Commit - Vitejs Vue - Tailwindcss - scss - Prettier - Eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'auto',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  pluginSearchDirs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.cjs',
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1
+    class="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md"
+  >
+    Hello world!
+  </h1>
+</template>
+
+<style scoped></style>
